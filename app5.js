8c0872770b27edfe37b603189d8f0efcf3d22eaf
史哉 <65520530+fuba238@users.noreply.github.com>
2021-07-20 06:58:53 +0000

    課題用に変更

--- app5.js
+++ app5.js
@@ -7,22 +7,13 @@ const db = new sqlite3.Database('sfv.db');
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  const message = "Hello world";
+  const message = "ストリータファイター5キャラクター情報へようこそ";
   res.render('show', {mes:message});
 });
-
-app.get("/db", (req, res) => {
-    db.serialize( () => {
-        db.all("select id, 都道府県, 人口, 大学 from example;", (error, row) => {
-            if( error ) {
-                res.render('show', {mes:"エラーです"});
-            }
-            res.render('db', {data:row});
-        })
-    })
-});
-
 app.get("/sfv", (req, res) => {
     db.serialize( () => {
         db.all("select id, 名前, 性別, 体力値, スタン値, 使用順位 from test;", (error, row) => {
@@ -33,6 +24,57 @@ app.get("/sfv", (req, res) => {
         })
     })
 });
+app.get("/use", (req, res) => {
+  let desc = "";
+  if( req.query.desc ) desc = " desc";
+  let sql = "select id, 名前, 性別, 体力値, スタン値, 使用順位 from test order by 使用順位" + desc + " limit " + req.query.pop + ";";
+  db.serialize( () => {
+    db.all(sql, (error, data) => {
+      if( error ) {
+        res.render('show', {mes:"エラーです"});
+      }
+      res.render('sfv', {data:data});
+    })
+  })
+})
+app.get("/hp", (req, res) => {
+  let desc = "";
+  if( req.query.desc ) desc = " desc";
+  let sql = "select id, 名前, 体力値, スタン値 from test order by 体力値" + desc + " limit " + req.query.pop + ";";
+  db.serialize( () => {
+    db.all(sql, (error, data) => {
+      if( error ) {
+        res.render('show', {mes:"エラーです"});
+      }
+      res.render('sfv_hpstan', {data:data});
+    })
+  })
+})
+app.get("/stan", (req, res) => {
+  let desc = "";
+  if( req.query.desc ) desc = " desc";
+  let sql = "select id, 名前, 体力値, スタン値 from test order by スタン値" + desc + " limit " + req.query.pop + ";";
+  db.serialize( () => {
+    db.all(sql, (error, data) => {
+      if( error ) {
+        res.render('show', {mes:"エラーです"});
+      }
+      res.render('sfv_hpstan', {data:data});
+    })
+  })
+})
+app.get("/gender", (req, res) => {
+  let sql = "select id, 名前, 性別 from test where 性別 = '" + req.query.gender + "';";
+  db.serialize( () => {
+    db.all(sql, (error, data) => {
+      if( error ) {
+        res.render('show', {mes:"エラーです"});
+      }
+      res.render('gender', {data:data});
+    })
+  })
+})
+
 app.use(function(req, res, next) {
   res.status(404).send('ページが見つかりません');
 });
